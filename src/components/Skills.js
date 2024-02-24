48c09114359e71dc3d4a1cd90b8e0464655788d9
sumair10 <sumairzahid123@gmail.com>
2024-02-24 21:25:01 +0400

    skill

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -34,26 +34,6 @@ import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 1,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 5,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 6,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 6,
-    },
-  };
-
   return (
     <section className="skill" id="skills">
       <div className="container">
